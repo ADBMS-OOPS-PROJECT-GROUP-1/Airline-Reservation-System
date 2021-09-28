7fe88b1f5dde9051d912f8239dae23fd010d4668
ANANYA B <74641663+Ananya31-tkm@users.noreply.github.com>
2021-09-28 23:32:28 +0530

    Demo_Insertion_To_Mongodb

---
+++ PROJECT/BACKEND/DB_Demo_Insert.java
@@ -0,0 +1,24 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class DB_Demo_Insert {
+    public static void main(String[] args){
+
+        // Creating a Mongo client
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        // Accessing the database
+        MongoDatabase database = mongo.getDatabase("Airlines");
+        // Retrieving a collection
+        MongoCollection<Document> collection = database.getCollection("Registration");
+
+        Document item = new Document("_id", new ObjectId());
+        item.append("Name","Anushree").append("Age",22).append("Contact",789669967).append("Gender","Female");
+        collection.insertOne(item);
+    }
+
+
+
+}
